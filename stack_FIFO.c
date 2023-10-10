6c7240ed6f7e4fb29e91b1ebbaf9db084a7251b3
vijay vekariya <vijayvekariya007@gmail.com>
2023-10-10 09:57:45 +0530

    10 oct

--- stack_FIFO.c
+++ stack_FIFO.c
@@ -18,7 +18,7 @@ int insertEnd(int data)
 
 
 
-int Delete()
+int Deletelast()
 {
     if(top < 0)
     {
@@ -31,11 +31,47 @@ int Delete()
     
 }
 
-int firstinsert()
+int firstinsert(int data)
 {
-    if()
+    if(top >= n-1)
+    {
+       printf("Stack is overflow...\n");
+    }
 
+    else if(top < 0)
+    {
+        top++;
+        a[top]=data;
+    }
+
+    else
+    {
+        top++;
+        
+        for(int i=top; i >=0; i--)
+        {
+            a[i]= a[i - 1];
+        } 
+        a[0]= data;
+    }
 }
+int firstdelete()
+{
+    if(top < 0)
+    {
+        printf("Stack is Empty....\n");
+    }
+    else 
+    {
+         for(int i=0; i<top; i++)
+         {
+            a[i]= a[i + 1];
+         }
+         top--;
+    }
+}    
+
+
 
 int display()
 {
@@ -49,6 +85,59 @@ int display()
 
 int main()
 {
-    
+    int choice;
+    int data;
+
+ do
+   {  
+        printf("\n\nStack Operation\n");
+        printf("Press 1. For Last location Insert data\n");
+        printf("Press 2. For Last location Delete data\n");
+        printf("Press 3. For Firtst Location Insert data\n");
+        printf("Press 4. For Firtst Location DElete data\n");
+       
+        printf("Press 5. For Dislpay data\n");
+        printf("Press 6. For Exit Program\n");
+     
+        printf("Enter Your Choise:");
+        scanf("%d",&choice);
+
+    switch(choice)
+    {
+      case 1:
+      printf("Enter the Element to insert last Location: ");
+      scanf("%d", &data);
+      insertEnd(data);
+      break;
+      
+      case 2:
+      Deletelast();
+      break;
+      
+      case 3:
+      printf("Enter the Element to insert First Location : ");
+      scanf("%d", &data);
+      firstinsert(data);
+      break;
+
+      case 4:
+      firstdelete();
+      
+      case 5:
+      display();
+      
+      case 6:
+      printf("\nExit From Program");
+      break;
+      
+      default:
+      printf("\nEnter Invalid Choice\n");
+      break;
+    }
+ }
+  while(choice !=6);    
+     
     
 }
+    
+    
